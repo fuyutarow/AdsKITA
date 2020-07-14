aaf8722ee9834db3d14c8f0a7a9c87a0cf99722b
fuyutarow <fuyutarow@gmail.com>
2020-07-15 02:13:18 +0900

    Try to use react-images-upload

--- web/src/views/Lab/index.tsx
+++ web/src/views/Lab/index.tsx
@@ -1,11 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import ImageUploader from 'react-images-upload';
+
+import { debug } from 'plugins/debug';
+
+const App = () => {
+  const [pictures, setPictures] = useState<Array<any>>([]);
+
+  // // const onDrop = ((files: File[], pictures: string[]) => {
+  // const onDrop = (files: File[], pictures: string[]) => {
+  const onDrop = (picture: any) => {
+    setPictures([...pictures, picture]);
+  };
+
+  return (
+    <>
+      <button onClick={e => {
+        debug(pictures);
+      }}>dbg</button>
+      <ImageUploader
+        withIcon={true}
+        onChange={onDrop}
+        imgExtension={['.jpg', '.gif', '.png', '.gif']}
+        maxFileSize={5242880}
+      />
+    </>
+  );
+};
 
 export default () => {
   const { id: adId } = useParams();
 
   return (
     <>
+      <App />
       <div>inner</div>
       <iframe
         {...{
