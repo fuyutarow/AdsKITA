47dec7570bc10165b0dfa3dfb1219ad3f6c936dc
fuyutarow <fuyutarow@gmail.com>
2020-07-15 03:34:38 +0900

    Try to use react-dropzone and js-base64

--- web/src/views/Lab/index.tsx
+++ web/src/views/Lab/index.tsx
@@ -1,9 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import ImageUploader from 'react-images-upload';
 
 import { debug, sampleBase64 } from 'plugins/debug';
+import { Base64 } from 'js-base64';
 
+import { useDropzone } from 'react-dropzone';
+
+const MyDropzone = () => {
+  const onDrop = useCallback((acceptedFiles: Array<File>) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+      reader.onload = () => {
+        const base64data = reader.result;
+        if (!base64data) return;
+        if (typeof base64data === 'string') return;
+
+        debug('reader result', base64data);
+        const base64string = Base64.fromUint8Array(new Uint8Array(base64data));
+        const dataURL = `data:image/png;base64,${base64string}`;
+        debug(dataURL);
+        // // Do whatever you want with the file contents
+        // const binaryStr = reader.result;
+        // debug(binaryStr);
+        // debug(sampleBase64);
+      };
+      reader.readAsArrayBuffer(file);
+      debug('reader', reader);
+    });
+
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} style={{
+      border: 'solid 2px red',
+      width: 300,
+      height: 300 * 1.6,
+    }}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  );
+};
 
 const App = () => {
   const [pictures, setPictures] = useState<Array<any>>([]);
@@ -30,15 +77,18 @@ const App = () => {
 };
 
 export default () => {
-  const { id: adId } = useParams();
 
   return (
     <>
+      <div>dropzone</div>
+      <MyDropzone />
+      {/* <App /> */}
       <img {...{
         src: sampleBase64,
         width: 300,
         height: 300,
-      }}/>
+      }} />
+      {/* <div>inner</div>
       <iframe
         {...{
           title: 'fmfm',
@@ -49,7 +99,7 @@ export default () => {
         style={{
           borderWidth: 0,
         }}
-      />
+      /> */}
       <div>gif</div>
       <iframe
         {...{
