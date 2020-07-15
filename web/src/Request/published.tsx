2f62bd93956d7b9397a0e058a9a36061abdc9259
fuyutarow <fuyutarow@gmail.com>
2020-07-15 22:09:03 +0900

    Add counter to Request/published again

--- web/src/Request/published.tsx
+++ web/src/Request/published.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-import { routes } from 'router';
+import firebase from 'firebase';
+
 import { db } from 'plugins/firebase';
+import { routes } from 'router';
 import { PublishedFlyer } from 'models';
 
 export default () => {
@@ -22,15 +24,17 @@ export default () => {
     [pubId],
   );
 
-  const IMG = () => flyer
-    ? (
-      <img {...{
-        width: 300,
-        height: 250,
-        src: flyer.imageURL,
-      }} />
-    )
-    : null;
+  // 表示回数カウンタ
+  useEffect(
+    () => {
+      if (!flyer) return;
+      if (window.parent.location.hostname !== flyer.targetDoamin) return;
+      db.collection('pubs').doc(flyer.id).collection('shards').doc('0').update({
+        displayCount: firebase.firestore.FieldValue.increment(1),
+      });
+    },
+    [flyer],
+  );
 
   const onClick = () => {
     if (!flyer) return;
@@ -38,12 +42,13 @@ export default () => {
 
     setClicked(true);
 
-    // // parent.locationで正しいドメインで広告表示されているか判定
-    // if (window.parent.location.hostname !== flyer.targetDoamin) return;
+    // parent.locationで正しいドメインで広告表示されているか判定
+    if (window.parent.location.hostname !== flyer.targetDoamin) return;
 
-    // db.collection('pubs').doc(pubId).collection('shards').doc('0').update({
-    //   clickCount: firebase.firestore.FieldValue.increment(1),
-    // });
+    // クリック回数カウンタ
+    db.collection('pubs').doc(pubId).collection('shards').doc('0').update({
+      clickCount: firebase.firestore.FieldValue.increment(1),
+    });
     const href = flyer?.linkURL || null;
     if (href) {
       history.push({
@@ -53,9 +58,20 @@ export default () => {
     }
   };
 
-  return (
-    <div onClick={onClick}>
-      <IMG />
-    </div>
-  );
+  const IMG: React.FC<{ flyer: PublishedFlyer }> = ({ flyer }) =>
+    (
+      <img {...{
+        width: 300,
+        height: 250,
+        src: flyer.imageURL,
+      }} />
+    );
+
+  return flyer
+    ? (
+      <div onClick={onClick}>
+        <IMG flyer={flyer} />
+      </div>
+    )
+    : null;
 };
