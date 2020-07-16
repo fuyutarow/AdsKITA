e9ebcff60c2f70abfcd90183d41ed4f7ad9857a9
fuyutarow <fuyutarow@gmail.com>
2020-07-16 20:23:44 +0900

    Fix routing: routes.home

--- web/src/views/Home/index.tsx
+++ web/src/views/Home/index.tsx
@@ -1,14 +1,23 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import { AuthContext } from 'contexts/auth';
 
 import { routes } from 'router';
 
 export default () => {
   const history = useHistory();
+  const auth = useContext(AuthContext);
 
-  useEffect(() => {
-    history.push(routes.requestList.path);
-  }, [history]);
+  useEffect(
+    () => {
+      if (auth) {
+        history.push(routes.requestList.path);
+      } else {
+        history.push(routes.login.path);
+      }
+    },
+    [auth, history],
+  );
 
   return null;
 
