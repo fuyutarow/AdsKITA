e139a8145ff58da974eef46b7766fe55b09aa1f8
fuyutarow <fuyutarow@gmail.com>
2020-07-19 03:30:11 +0900

    Add plugins/stripe

--- web/src/components/Login.tsx
+++ web/src/components/Login.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { StyledFirebaseAuth } from 'react-firebaseui';
+import { useHistory } from 'react-router-dom';
 // import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
 
 import { auth } from 'plugins/firebase';
+import { config as stripeConfig, badge as stripeBadge } from 'plugins/stripe';
+
+import Button from '@material-ui/core/Button';
 
 const uiConfig = {
   signInFlow: 'popup',
@@ -21,10 +25,26 @@ const uiConfig = {
 };
 
 export default () => {
+  const history = useHistory();
 
   return (
-    <div>
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
-    </div>
+    <>
+      <div>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+      </div>
+      <div >
+        <Button
+          variant='contained'
+          onClick={e => {
+            window.location.href = `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${stripeConfig.ca}&scope=read_write`;
+          }}
+        >
+          <span style={{ padding: '0 10px 0 0' }}>
+            決済
+          </span>
+          <img src={stripeBadge} />
+        </Button>
+      </div>
+    </>
   );
 };
