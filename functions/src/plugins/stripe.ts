0546b2934297ac036d9baefa74895b18eca4fa08
fuyutarow <fuyutarow@gmail.com>
2020-07-23 06:02:27 +0900

    Add functions: stripe acctoun retrieve

---
+++ functions/src/plugins/stripe.ts
@@ -0,0 +1,15 @@
+
+import Stripe from 'stripe';
+
+export const config = {
+  ca: 'ca_HfaNc9U8GWsiXv6FPEGfb4SRsgjaHYyE',
+  publicKey: 'pk_test_51H6EpFL2CNVURAeAMUWXtJwqjTK9EX42CNyp4AkniQSV76XUpYLhQ4J0PMjTJSDmv8JoEPwI9mu156yFUqQ0HgMH00pVHcAF6g',
+  secretKey: 'sk_test_51H6EpFL2CNVURAeAwVdwQq66zEQZ3YWpscxDtIVcLzNCfRpJPwIGh3xgMRXWDP6tCjQj5PjnkDRbQr06FHPKSpIC007MpVo92T',
+};
+
+export const stripe = new Stripe(
+  config.secretKey,
+  {
+    apiVersion: '2020-03-02',
+  }
+);
