05463d1c2d028e231152155eb8f38f7ec2d55084
fuyutarow <fuyutarow@gmail.com>
2020-07-22 22:20:47 +0900

    Add: Retrieve account

---
+++ functions/src/index.ts
@@ -0,0 +1,21 @@
+import * as functions from 'firebase-functions';
+import Stripe from 'stripe';
+import { stripe } from './plugins/stripe';
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
+});
+
+export const retrieve = functions.https.onCall(async (data, context) => {
+  const args = data as {
+    id: string,
+    params?: Stripe.AccountRetrieveParams | undefined,
+    options?: Stripe.RequestOptions | undefined
+  };
+  const account = await stripe.accounts.retrieve(args.id, args.params, args.options);
+  return {
+    data: {
+      account: account,
+    },
+  };
+});
---
+++ server/ttt.ts
@@ -0,0 +1,26 @@
+// import Stripe from 'stripe';
+import { loadStripe } from '@stripe/stripe-js';
+
+
+
+export const config = {
+  ca: 'ca_HfaNc9U8GWsiXv6FPEGfb4SRsgjaHYyE',
+  publicKey: 'pk_test_51H6EpFL2CNVURAeAMUWXtJwqjTK9EX42CNyp4AkniQSV76XUpYLhQ4J0PMjTJSDmv8JoEPwI9mu156yFUqQ0HgMH00pVHcAF6g',
+  secretKey: 'sk_test_51H6EpFL2CNVURAeAwVdwQq66zEQZ3YWpscxDtIVcLzNCfRpJPwIGh3xgMRXWDP6tCjQj5PjnkDRbQr06FHPKSpIC007MpVo92T',
+};
+
+// export const stripe = new Stripe(
+//   config.secretKey,
+//   {
+//     apiVersion: '2020-03-02',
+//   },
+// );
+
+const stripe = await loadStripe(config.publicKey)
+
+(async () => {
+  const acc = await stripe.accounts.retrieve(
+    'acct_1H6EpFL2CNVURAeA',
+  );
+  console.log(acc);
+})();
