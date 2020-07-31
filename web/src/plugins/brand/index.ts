fe509c385f2000ebbb372e34f461e8f7c20975a5
fuyutarow <fuyutarow@gmail.com>
2020-07-31 20:38:46 +0900

    Update views/Policy

---
+++ web/src/plugins/brand/index.ts
@@ -0,0 +1,29 @@
+
+export const brandColors = {
+  orange: '#F94E0D',
+  ground: '#FFFDD0',
+  // khaki: '#808068',
+  khaki: '#353535',
+};
+
+export const logo = '/logo.png';
+
+export const icons = {
+  192: '/icons/192x192.png',
+  512: '/icons/512x512.png',
+};
+
+export const law = {
+  '販売業者': 'AdsKITA',
+  '運営責任者': '福田 優太朗',
+  '住所': '準備中',
+  '注文方法': 'インターネット上からご利用できます',
+  '支払方法': 'クレジットカード',
+};
+
+export const contactFormLink =
+  'https://docs.google.com/forms/d/e/1FAIpQLSffHGpTxBYjtk0qma869D01yf8rOgIG5tCr8bh8QFcSToDYMg/viewform';
+
+export const terms: Record<string, { body: string }> = require('./terms.json');
+
+export const policy: Record<string, { body: string }> = require('./policy.json');
